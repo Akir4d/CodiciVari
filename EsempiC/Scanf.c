b96c87bce19e7079e819c503225b833f63491ed7
Paolo Rampino <liadserv@gmail.com>
2025-07-08 05:02:56 -0400

    UP

--- EsempiC/Scanf.c
+++ EsempiC/Scanf.c
@@ -18,6 +18,10 @@ int main()
     il secondo e' l'indirizzo di memoria della variabile che si ottiene anteponendo & alla varibile.
     */
     scanf("%d", &primo_numero);
+    /*
+    printf accetta piu' parametri, il primo deve essere una stringa formattata in modo che capisca come
+    deve sostituire il testo con i valori dei paramentri successivi, i valori %d,%x etc sono i medesimi di scanf
+    */
     printf("\nhai inserito %d che si trova all'indirizzo di memoria 0x%x", primo_numero, &primo_numero);
     return primo_numero;
 }
