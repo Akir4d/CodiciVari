c92a7a3f6ac45e965bfeac08e15ca5d79bc79bdd
Paolo Rampino <liadserv@gmail.com>
2025-07-08 11:57:01 -0400

    UP

--- EsempiC/funzioni.c
+++ EsempiC/funzioni.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 
-
-int somma(int a, int b){
+int somma(int a, int b)
+{
     int c;
     c = a + b;
     return c;
 }
 
-void stampa_somma(int a, int b){
-    printf("La somma e' %d", somma(a,b));
+void stampa_somma(int a, int b)
+{
+    int som = somma(a, b);
+    if (som < a && som < b)
+    {
+        printf("Qualcosa e' andato storto! %d + %d = %d?", a,b,som);
+    }
+    else
+    {
+        printf("La somma %d + %d = %d", a,b,som);
+    }
 }
 
-int main(){
-    stampa_somma(89,90);
+int main()
+{
+    int a,b;
+    printf("\nInserisci a: ");
+    scanf("%d", &a);
+    printf("\nInserisci b: ");
+    scanf("%d", &b);
+    stampa_somma(a, b);
     return 0;
 }
