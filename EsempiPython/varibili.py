b99bac59303cd8a73c1c8419e09de9d03cccc5a7
Paolo Rampino <liadserv@gmail.com>
2025-07-09 04:50:22 -0400

    UP

---
+++ EsempiPython/varibili.py
@@ -0,0 +1,39 @@
+# non posso scrivere
+# 1Marta
+# deve iniziare per a-zA-Z o _
+
+# Questo non e' un errore, ma per convezione e' usato per le classi
+MartaPrima=1
+# le varibili per convenzione devono essere snake_case, questo e' corretto
+marta_prima=1
+
+#Ad ogni modo le varibili sono case sensitive quindi Marta e' diverso da marta
+Marta = 0
+marta = 1
+
+# print(Marta, marta)
+# intero
+x=10
+# virgola mobile
+y=3.14
+# stringa
+nome="Alice"
+# la stringa in fase di dichiarazione puo' avere diversi formati basta anteporre una lettera
+# prima delle virgolette, se metto abilito la possibilita' di inserire funzioni o varibili in modo semplice
+nome=f"Alice {y}"
+print(nome) # Alice 3.14
+
+# Boleano True/False
+utente_attivo=True
+
+# lista
+numeri = [1,2,3,4]
+
+# tuple
+coordinate = (10,20)
+
+#dizionari
+utente = {"nome": "Alice", "eta": 25}
+
+x = x + y
+print(x)
