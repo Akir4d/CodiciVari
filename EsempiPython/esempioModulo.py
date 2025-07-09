4c93e7435d4edc10169926afe9f6ede34c955dbe
Paolo Rampino <liadserv@gmail.com>
2025-07-09 09:34:37 -0400

    UP

---
+++ EsempiPython/esempioModulo.py
@@ -0,0 +1,19 @@
+#import libreriaMatematica
+
+#print(libreriaMatematica.quadrato(2))
+
+#from libreriaMatematica import quadrato
+
+#print(quadrato(3))
+
+#from libreriaMatematica import *
+
+#print(quadrato(3))
+
+#import libreriaMatematica as lm
+
+#print(lm.quadrato(5))
+
+import libreria.libreriaMatematica as lm
+
+print(lm.quadrato(5))
