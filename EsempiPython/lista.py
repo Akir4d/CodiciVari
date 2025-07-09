4c27d5aa08600f78bdb3bd65b30399bac5e1b946
Paolo Rampino <liadserv@gmail.com>
2025-07-09 05:31:35 -0400

    UP

---
+++ EsempiPython/lista.py
@@ -0,0 +1,20 @@
+lista = [1, "ciao", 3.14, [1, 2, 3]]
+print(lista[0])  # Stampa: 1
+print(lista[1])  # Stampa: ciao
+print(lista[2])  # Stampa: 3.14
+print(lista[3])  # Stampa: [1, 2, 3]
+print(lista[3][0])  # Stampa: 1 (accesso al primo elemento della lista interna)
+
+lista.pop()
+print(lista)
+## lista.remove(elemento da rimuovere)
+lista.remove("ciao")
+print(lista)
+
+## lista.append() Mette un elemento alla fine
+lista.append([1,2,3,6])
+print(lista)
+
+## lista.insert(elemento dopo quale inserire, elemento nuovo)
+lista.insert(0,"ciao")
+print(lista)
